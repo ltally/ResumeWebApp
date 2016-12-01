68b24360548265e1b2c29ced0ba4bfa39db932b7
ltally <tally@sonoma.edu>
2016-11-30 17:58:33 -0800

    Lab 11 school addresses

--- model/school_dal.js
+++ model/school_dal.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection(db.config);
 */
 
 exports.getAll = function(callback) {
-    var query = 'SELECT * FROM school;';
+    var query = 'SELECT * FROM school_view;';
 
     connection.query(query, function(err, result) {
         callback(err, result);
