a8fe1c60692ea7ab8a83ee75757d8a46774df89c
ltally <tally@sonoma.edu>
2016-12-06 14:12:14 -0800

    Lab 11

--- routes/company_routes.js
+++ routes/company_routes.js
@@ -36,4 +36,59 @@ router.get('/', function(req, res){
     }
 });
 
+// Return the add a new company form
+router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
+    company_dal.getAll(function(err,result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.render('company/companyAdd');
+        }
+    });
+});
+
+// insert a company record
+router.get('insert', function(req, res){
+    //simple validation
+    if(req.query.name == null) {
+        res.send('Name must be provided.');
+    }
+    else if(req.query.company_id == null){
+        res.send('A company must be selected');
+    }
+    else {
+        // passing all the query parameters (req.query) to the insert function instead of each individually
+        company_dal.insert(req.query, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/company/all');
+            }
+        });
+    }
+});
+
+// Delete a company for the given company_id
+router.get ('/delete', function(req, res){
+    if(req.query.company_id == null) {
+        res.send('company_id is null');
+    }
+    else {
+        company_dal.delete(req.query.company_id, function(err,result){
+            if(err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/company/all');
+            }
+        });
+    }
+});
+
+
 module.exports = router;
